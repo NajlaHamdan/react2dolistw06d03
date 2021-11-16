09d11cb601f376812de48d20012d09903c9ca949
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-11-16 23:16:30 +0300

    post function

--- src/components/CreateItem/index.js
+++ src/components/CreateItem/index.js
@@ -3,6 +3,15 @@ import axios from 'axios';
 const BASE_URL = "http://localhost:4000";
 
 const CreateItem = () => {
+    function createItem(item){
+        axios.post(`${BASE_URL}/`),{
+            result:item,
+        }.then(function(res){
+
+        }).catch(function(err){
+            console.log(err);
+        })
+    }
     return (
         <div>
             
